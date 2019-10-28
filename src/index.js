c5159f6be87e28df4f9319e41c16019a470afb18
Andrew Duong <duongandrews@gmail.com>
2019-10-28 12:12:09 -0400

    Re-factored boilerplate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Boilerplate</h1>
-      </div>
-    );
-  }
-}
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
