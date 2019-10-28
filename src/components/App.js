1c9d81f3aed0499d5b15c34b0278b92f4d323c78
Andrew Duong <duongandrews@gmail.com>
2019-10-28 13:27:47 -0400

    Setup Google Maps API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import MapContainer from "./Map";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Boilerplate</h1>
+        <MapContainer />
       </div>
     );
   }
---
+++ src/components/Map.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends React.Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Map google={this.props.google} onClick={this.onMapClicked}>
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ""
+})(MapContainer);
