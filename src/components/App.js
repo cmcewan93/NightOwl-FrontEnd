5de87669ec9050fa5c0fc3438b6d40538bc0d17e
Andrew Duong <duongandrews@gmail.com>
2019-10-29 12:45:18 -0400

    Added a Search Bar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 // import MapContainer from "./Map";
-import Login from "./Login";
+// import Login from "./Login";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Login />
+        <SearchBar />
       </div>
     );
   }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
