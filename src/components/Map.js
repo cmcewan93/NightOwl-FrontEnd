50ed7354f162d66a9ef4894bbf70a04483016d27
Andrew Duong <duongandrews@gmail.com>
2019-10-30 17:25:19 -0400

    Mapped through the database to dynamically render Markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,21 +10,10 @@ const mapStyles = {
   height: "100%"
 };
 
-// const setMarker = (posts) => {
-//   for(let i = 0; i < posts.length; i++) {
-//     <Marker position={{
-//       lat: posts[i].latitude,
-//       lng: posts[i].longitude
-//     }}
-//     name={posts[i].name}
-//   }
-// }
-
 export class MapContainer extends React.Component {
   async componentDidMount() {
     const { data } = await Axios.get(`/api/venues`);
-    this.setState({ posts: data });
-    // console.log("this is a test", this.state.posts);
+    this.setState({ markers: data });
   }
 
   state = {
@@ -34,7 +23,7 @@ export class MapContainer extends React.Component {
     defaultLat: 43.644175,
     defaultLng: -79.402204,
     errorMessage: "",
-    posts: []
+    markers: []
   };
 
   // componentDidMount() {
@@ -79,10 +68,25 @@ export class MapContainer extends React.Component {
     }
   };
 
+  // Renders all of the markers
+  renderMarkers() {
+    return this.state.markers.map(marker => {
+      return (
+        <Marker
+          key={marker.id}
+          onClick={this.onMarkerClick}
+          position={{ lat: marker.latitude, lng: marker.longitude }}
+          name={marker.name}
+          icon={{
+            url: "/pint.svg",
+            scaledSize: new window.google.maps.Size(30, 30)
+          }}
+        />
+      );
+    });
+  }
+
   render() {
-    const { posts } = this.state;
-    console.log("this is a test", posts);
-    // console.log(this.state.posts);
     return (
       <Map
         google={this.props.google}
@@ -94,13 +98,6 @@ export class MapContainer extends React.Component {
         }}
         zoom={14}
       >
-        {/* posts.map(post => {
-  <Marker position={{
-    lat: post.latitude,
-    lng: post.longitude
-  }}
-}) */}
-
         <Marker
           onClick={this.onMarkerClick}
           name={"Current Location"}
@@ -113,30 +110,9 @@ export class MapContainer extends React.Component {
             scaledSize: new window.google.maps.Size(25, 25)
           }}
         />
-        {/* <Marker
-          onClick={this.onMarkerClick}
-          name={"Belfast Love"}
-          position={{
-            lat: 43.6448919,
-            lng: -79.4006397
-          }}
-          icon={{
-            url: "/pint.svg",
-            scaledSize: new window.google.maps.Size(30, 30)
-          }}
-        /> */}
-        {/* <Marker
-          onClick={this.onMarkerClick}
-          name={"Petty Cash"}
-          position={{
-            lat: this.state.lat,
-            lng: this.state.lng
-          }}
-          icon={{
-            url: "/pint.svg",
-            scaledSize: new window.google.maps.Size(30, 30)
-          }}
-        /> */}
+        {/* Rendering all the Markers */}
+        {this.renderMarkers()}
+
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
