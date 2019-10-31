80bfa862c92ff7a39b645ac92c61aa5a64deffa3
allisonnnnnn <allisonchen1130@gmail.com>
2019-10-31 13:37:03 -0400

    bar-view page implemented

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "/Users/mengting/lighthouse/final-front-end/src/components/login.css";
-// import Avatar from "@material-ui/core/Avatar";
+import "../components/login.css";
+
 import Logo from "./Logo";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -10,7 +10,6 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
