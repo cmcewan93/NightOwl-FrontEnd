6660e80304c4c02b5869606d3cd8a300ee825d8d
Colin McEwan <colin-mcewan@hotmail.com>
2019-10-31 19:54:45 -0400

    added styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./Login";
 import MapView from "./MapView";
 // import Burger from "./Burger";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
   render() {
