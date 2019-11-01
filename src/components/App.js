fee1227e7d4463543d262abc137b38b49b922914
allison <allisonchen1130@gmail.com>
2019-11-01 19:49:08 -0400

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import MapView from "./MapView";
 // import Burger from "./Burger";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import BarviewList from "./BarviewList";
+import Camera from "./Camera";
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <MapView />
+        <BarviewList />
       </>
     );
   }
