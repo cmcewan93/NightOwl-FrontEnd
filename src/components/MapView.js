7dd261eee6806e4ab3f6fa94fda3951de768556f
Andrew Duong <duongandrews@gmail.com>
2019-11-01 14:58:50 -0400

    Fixed State for Filters

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import Axios from "axios";
 
 import Map from "./Map";
 import HeaderContainer from "./HeaderContainer";
 import FilterContainer from "./FilterContainer";
 
-class MapView extends React.Component {
+class MapView extends Component {
   state = {
-    markers: []
+    markers: [],
+    filteredMarkers: []
   };
 
   async componentDidMount() {
     const { data } = await Axios.get(`/api/venues`);
-    this.setState({ markers: data });
+    this.setState({ markers: data, filteredMarkers: data });
   }
 
   updateMarkers = async type => {
     const { data } = await Axios.get(`/api/filters/${type}`);
+    console.log("@@@@", data);
     const filteredIds = data.map(item => item.venue_id);
     const filterData = this.state.markers.filter(venue =>
       filteredIds.includes(venue.id)
     );
-
-    this.setState({ markers: filterData });
+    console.log("FILTERDATA", filterData);
+    this.setState({ filteredMarkers: filterData });
   };
 
   _allMarkers = () => {
     Axios.get(`/api/venues`).then(response => {
-      this.setState({ markers: response.data });
+      this.setState({ filteredMarkers: response.data, markers: response.data });
     });
   };
 
@@ -36,7 +38,7 @@ class MapView extends React.Component {
     return (
       <>
         <HeaderContainer />
-        <Map markers={this.state.markers} />
+        <Map markers={this.state.filteredMarkers} />
         <FilterContainer
           updateMarkers={this.updateMarkers}
           allMarkers={this._allMarkers}
