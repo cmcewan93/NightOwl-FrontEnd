c1eadde1c58b50016cb026b5af25f5f0b5942c2c
allison <allisonchen1130@gmail.com>
2019-11-02 17:09:32 -0400

    contact info

--- src/components/BarviewList.js
+++ src/components/BarviewList.js
@@ -8,6 +8,11 @@ import Axios from "axios";
 const AgeIcon = () => {
   return (
     <>
+      <i className="phone icon"></i>
+      <p className="phone-number">Phone number: 20</p>
+      <i className="exchange icon"></i>
+      <p className="address">Address: 20</p>
+
       <i className="smile outline icon"></i>
       <p className="age">Average age: 20</p>
       <i className="users usersa icon"></i>
@@ -15,9 +20,9 @@ const AgeIcon = () => {
       <i className="dollar dollara sign icon"></i>
       <p className="price">Average price: 20</p>
       <i className="music musica icon"></i>
-      <p className="music-type">Average price: 20</p>
+      <p className="music-type">Music type: 20</p>
       <i className="user secret icon"></i>
-      <p className="dress-code">Average price: 20</p>
+      <p className="dress-code">Dress code: 20</p>
     </>
   );
 };
