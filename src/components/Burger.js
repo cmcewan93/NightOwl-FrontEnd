084531c32ee70c29f8933b13502cd96ed4cbbd76
Andrew Duong <duongandrews@gmail.com>
2019-11-03 13:34:45 -0500

    Added CSS for the mapview

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -7,14 +7,37 @@ import {
   Redirect
 } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
+import { ReactComponent as Owl } from "../images/owl.svg";
+import { ReactComponent as Home } from "../images/house.svg";
+import { ReactComponent as Logout } from "../images/logout.svg";
+
+const owlAlignment = {
+  position: "fixed",
+  height: "90px",
+  width: "90px",
+  right: "7px",
+  bottom: "15px"
+};
+
+const imageSize = {
+  height: "40px",
+  width: "40px"
+};
 
 export default props => {
   return (
     // Pass on our props
     <Menu {...props} right>
-      <Link to={"/"}>Logout</Link>
-      <Link to={"/map"}>Home</Link>
+      <Link to={"/"}>
+        <Logout style={imageSize} />
+        Logout
+      </Link>
+      <Link to={"/map"}>
+        <Home style={imageSize} />
+        Home
+      </Link>
       <Link to={"/bar"}>Test Bar View</Link>
+      <Owl style={owlAlignment} />
     </Menu>
   );
 };
