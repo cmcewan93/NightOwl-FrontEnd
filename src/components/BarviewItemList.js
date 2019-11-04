c5a48188b1ff4c1ce9bbc97855c8c3c67080b68e
Andrew Duong <duongandrews@gmail.com>
2019-11-04 12:13:36 -0500

    Tested out new components

---
+++ src/components/BarviewItemList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactComponent as Test } from "../images/glass.svg";
+
+class BarviewItems extends React.Component {
+  render() {
+    return (
+      <>
+      Test
+      Hello
+      </>
+    )
+  }
+}
--- src/components/BarviewList.js
+++ src/components/BarviewList.js
@@ -5,27 +5,7 @@ import Burger from "./Burger";
 import "./BarviewList.css";
 import Axios from "axios";
 
-const AgeIcon = () => {
-  return (
-    <>
-      <i className="phone icon"></i>
-      <p className="phone-number">Phone number: 20</p>
-      <i className="exchange icon"></i>
-      <p className="address">Address: 20</p>
 
-      <i className="smile outline icon"></i>
-      <p className="age">Average age: 20</p>
-      <i className="users usersa icon"></i>
-      <p className="linesize">Line size: 20</p>
-      <i className="dollar dollara sign icon"></i>
-      <p className="price">Average price: 20</p>
-      <i className="music musica icon"></i>
-      <p className="music-type">Music type: 20</p>
-      <i className="user secret icon"></i>
-      <p className="dress-code">Dress code: 20</p>
-    </>
-  );
-};
 
 const Barname = () => {
   return <h3 className="barname">Barname</h3>;
@@ -36,9 +16,7 @@ const BackButton = () => {
 };
 
 class BarviewList extends React.Component {
-  state = {
-    ratio: []
-  };
+ 
 
   updateChart = async () => {
     const { data } = await Axios.get(`/api/venues/`);
