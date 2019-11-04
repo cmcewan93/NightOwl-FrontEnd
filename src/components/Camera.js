e1736f3f214305b3f628097e6f765015bb4ecae0
Andrew Duong <duongandrews@gmail.com>
2019-11-04 15:46:37 -0500

    Changed Camera

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,21 +1,122 @@
-import React, { Component } from "react";
-import Camera from "react-html5-camera-photo";
-import "react-html5-camera-photo/build/css/index.css";
+import React from "react";
+import CameraPhoto, { FACING_MODES } from "jslib-html5-camera-photo";
 
-class CameraComponent extends Component {
-  onTakePhoto(dataUri) {}
+class Camera extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.cameraPhoto = null;
+    this.videoRef = React.createRef();
+    this.state = {
+      dataUri: ""
+    };
+  }
+
+  componentDidMount() {
+    // We need to instantiate CameraPhoto inside componentDidMount because we
+    // need the refs.video to get the videoElement so the component has to be
+    // mounted.
+    this.cameraPhoto = new CameraPhoto(this.videoRef.current);
+  }
+
+  startCamera(idealFacingMode, idealResolution) {
+    this.cameraPhoto
+      .startCamera(idealFacingMode, idealResolution)
+      .then(() => {
+        console.log("camera is started !");
+      })
+      .catch(error => {
+        console.error("Camera not started!", error);
+      });
+  }
+
+  startCameraMaxResolution(idealFacingMode) {
+    this.cameraPhoto
+      .startCameraMaxResolution(idealFacingMode)
+      .then(() => {
+        console.log("camera is started !");
+      })
+      .catch(error => {
+        console.error("Camera not started!", error);
+      });
+  }
+
+  takePhoto() {
+    const config = {
+      sizeFactor: 1
+    };
+
+    let dataUri = this.cameraPhoto.getDataUri(config);
+    this.setState({ dataUri });
+  }
+
+  stopCamera() {
+    this.cameraPhoto
+      .stopCamera()
+      .then(() => {
+        console.log("Camera stoped!");
+      })
+      .catch(error => {
+        console.log("No camera to stop!:", error);
+      });
+  }
 
   render() {
     return (
-      <div className="App">
-        <Camera
-          onTakePhoto={dataUri => {
-            this.onTakePhoto(dataUri);
+      <div>
+        <button
+          onClick={() => {
+            let facingMode = FACING_MODES.ENVIRONMENT;
+            let idealResolution = { width: 640, height: 480 };
+            this.startCamera(facingMode, idealResolution);
           }}
-        />
+        >
+          {" "}
+          Start environment facingMode resolution ideal 640 by 480{" "}
+        </button>
+
+        <button
+          onClick={() => {
+            let facingMode = FACING_MODES.USER;
+            this.startCamera(facingMode, {});
+          }}
+        >
+          {" "}
+          Start user facingMode resolution default{" "}
+        </button>
+
+        <button
+          onClick={() => {
+            let facingMode = FACING_MODES.USER;
+            this.startCameraMaxResolution(facingMode);
+          }}
+        >
+          {" "}
+          Start user facingMode resolution maximum{" "}
+        </button>
+
+        <button
+          onClick={() => {
+            this.takePhoto();
+          }}
+        >
+          {" "}
+          Take photo{" "}
+        </button>
+
+        <button
+          onClick={() => {
+            this.stopCamera();
+          }}
+        >
+          {" "}
+          Stop{" "}
+        </button>
+
+        <video ref={this.videoRef} autoPlay="true" />
+        <img alt="imgCamera" src={this.state.dataUri} />
       </div>
     );
   }
 }
 
-export default CameraComponent;
+export default Camera;
