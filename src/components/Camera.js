b47b44a4f4f2e013dbb8490f729c8e8e4eb25c75
allison <allisonchen1130@gmail.com>
2019-11-05 23:53:19 -0500

    added style for image

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,18 +1,56 @@
 import React, { Component } from "react";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
+import Axios from "axios";
 
 class CameraComponent extends Component {
-  onTakePhoto(dataUri) {}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataUri: ""
+    };
+  }
+
+  onTakePhoto(dataUri) {
+    // Do stuff with the dataUri photo...
+    console.log(dataUri);
+    this.setState({
+      dataUri
+    });
+  }
+
+  submitPhoto = () => {
+    Axios.post("/api/reviews/upload", {
+      body: JSON.stringify(this.state.dataUri),
+      headers: {
+        "Content-Type": "image/jpg"
+      }
+    });
+  };
 
   render() {
     return (
-      <div className="App">
-        <Camera
-          onTakePhoto={dataUri => {
-            this.onTakePhoto(dataUri);
-          }}
-        />
+      <div style={{}}>
+        {this.state.dataUri ? (
+          <div>
+            <img alt="" src={this.state.dataUri} />
+            <button onClick={() => this.submitPhoto()}>submit</button>
+          </div>
+        ) : (
+          <Camera
+            onTakePhoto={dataUri => {
+              this.onTakePhoto(dataUri);
+            }}
+            idealResolution={{ width: 640, height: 320 }}
+            isMaxResolution={false}
+            isImageMirror={false}
+            isSilentMode={true}
+            isDisplayStartCameraError={true}
+            isFullscreen={false}
+            sizeFactor={0.5}
+          />
+        )}
       </div>
     );
   }
